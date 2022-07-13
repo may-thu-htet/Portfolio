5b86702f7d5d84965ff5dcb9138c9eaa1b023b35
may-thu-htet <maythuhtet2892@gmail.com>
2022-07-13 21:31:03 +0900

    work in progress page and animation added

---
+++ mth-portfolio/src/views/WorkInProgress.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1 class="header">Still working in progress</h1>
+  <div class="col-md-3">
+    <svg-animation class="img-fluid"></svg-animation>
+  </div>
+</template>
+<script setup>
+import SvgAnimation from "./SvgAnimation.vue";
+</script>
+<style scoped>
+.header {
+  color: #004f70;
+  position: absolute;
+  width: 570px;
+  height: 89px;
+  left: 435px;
+  top: 43px;
+}
+.img-fluid {
+  position: absolute;
+  width: 300px;
+  height: 400px;
+  left: 480px;
+  top: 120px;
+}
+</style>
