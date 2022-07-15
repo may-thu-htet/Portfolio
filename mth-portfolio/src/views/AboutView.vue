1555bc0d2260b806601bd850185826ea83226999
may-thu-htet <maythuhtet2892@gmail.com>
2022-07-15 19:55:40 +0900

    about page

--- mth-portfolio/src/views/AboutView.vue
+++ mth-portfolio/src/views/AboutView.vue
@@ -1,15 +1,89 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <body style="background-color: #d8cbfb">
+    <h1 class="about">Introduction (自己紹介)</h1>
+    <div>
+      <p>
+        I had finished my undergraduate degree in February,2015 and decided to
+        study abroad. Therefore, I started to prepare the language skill
+        required and then went to UK and attended post-gradurate master program
+        at Newcastle University. After I have finished my graduation, I started
+        working as a freelance Naval Architect. After a little while, I felt
+        like I want to challenge more and decided to come to Japan to work as a
+        design engineer as Toyota Kohki Co.,Ltd. I tried at the aforemensioned
+        company and I even went to factory for training as I want to design the
+        formwork from scratch. The following photos were taken when I went to
+        the factory and trying to understand the assembly and welding process so
+        that I can apply in design consideration.
+      </p>
+      <div>
+        <img src="../welding photos/IMG_6441.JPG" alt="pic 1" />
+        <img src="../welding photos/IMG_6442.JPG" alt="pic 2" />
+        <img src="../welding photos/IMG_6444.JPG" alt="pic 3" />
+        <img src="../welding photos/IMG_6454.JPG" alt="pic 4" />
+      </div>
+      <p>
+        After a while, I am thinking about what I want to do in the future
+        career and the result is that I want to deal with the creative design,
+        implementing simple and artistic web app as well as moblie app. And
+        hence I started learning basic HTML,CSS and JavaScript in October, 2011.
+        From that, I moving to self-study UIUX design using Figma and design
+        concept and I am really enjoying it. As my hobby is creating art and I
+        have been doing painting and drawing since my childhood. I have drawn a
+        lot of portraits, acrylic and watercolor paintings, modern paintings
+        using acrylics, color pencils and pastels. Resently, I started learning
+        digital drawing using Procreate App. I also want to learn paint using
+        oil which is on my next basket list. The following pictures are some of
+        my art works throughout my life until now.
+      </p>
+      <div>
+        <img src="../Painting/Mt.Fuji.jpg" alt="paint1" />
+        <img src="../Painting/Splashing cloud.jpg" alt="paint2" />
+        <img src="../Painting/War and Peace.jpg" alt="paint3" />
+        <img src="../Painting/Tharapa gate.jpg" alt="paint4" />
+        <img src="../Painting/Fiona.jpg" alt="paint5" />
+        <img src="../Painting/Comy.jpg" alt="paint6" />
+        <img src="../Painting/Taylor Swift .jpg" alt="paint7" />
+        <img src="../Painting/The Eyes.jpg" alt="paint8" />
+      </div>
+    </div>
+  </body>
 </template>
 
 <style>
 @media (min-width: 1024px) {
   .about {
-    min-height: 100vh;
     display: flex;
     align-items: center;
+    margin: 1rem;
+    color: #434344;
+    background-color: #d8cbfb;
+    margin-top: 0;
+    margin-bottom: 0;
   }
+}
+h1 {
+  text-align: center;
+  padding-top: 1rem;
+}
+p {
+  font-family: "Lato", sans-serif;
+  color: #434344;
+  background-color: #d8cbfb;
+  font-size: large;
+  margin-bottom: 1rem;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  padding-left: 1rem;
+  font-display: justify;
+  justify-content: space-evenly;
+}
+img {
+  width: 25%;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 10px 20px;
+  border-radius: 15%;
+  height: auto;
+  justify-content: space-evenly;
 }
 </style>
